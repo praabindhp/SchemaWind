458ab19b4849d53160fbcafe6ef72fbcb0155388
praabindh <praabindh@codingmart.com>
2023-03-06 12:26:47 +0530

    Worked With SchemaWind

--- SQLs/Instagram-MySQL.sql
+++ SQLs/Instagram-MySQL.sql
@@ -1,12 +1,13 @@
-CREATE TABLE `User`(
-    `id` CHAR(36) NOT NULL,
-    `name` VARCHAR(255) NOT NULL,
-    `phoneNumber` VARCHAR(255) NOT NULL,
-    `email` VARCHAR(255) NOT NULL,
-    `address` VARCHAR(255) NOT NULL
+CREATE TABLE `Saved Post`(
+    `postId` INT NULL,
+    `userId` INT NULL
 );
 ALTER TABLE
-    `User` ADD PRIMARY KEY `user_id_primary`(`id`);
+    `Saved Post` ADD PRIMARY KEY `saved post_postid_userid_primary`(`postId`, `userId`);
+ALTER TABLE
+    `Saved Post` ADD UNIQUE `saved post_postid_unique`(`postId`);
+ALTER TABLE
+    `Saved Post` ADD UNIQUE `saved post_userid_unique`(`userId`);
 CREATE TABLE `Photo`(
     `id` CHAR(36) NOT NULL,
     `albumID` CHAR(36) NOT NULL,
@@ -21,14 +22,57 @@ CREATE TABLE `Photo`(
 );
 ALTER TABLE
     `Photo` ADD PRIMARY KEY `photo_id_primary`(`id`);
-CREATE TABLE `Comment`(
+CREATE TABLE `Hash Tag`(
+    `hashTagId` INT NULL,
+    `postId` INT NULL,
+    `content` VARCHAR(255) NULL
+);
+CREATE TABLE `Profile`(
+    `userId` INT NULL,
+    `photourl` VARCHAR(255) NULL,
+    `publicType` TINYINT(1) NULL,
+    `bio` VARCHAR(255) NULL
+);
+CREATE TABLE `Post Comment`(
     `id` CHAR(36) NOT NULL,
     `photoID` CHAR(36) NOT NULL,
     `postDate` DATE NOT NULL,
     `content` VARCHAR(255) NOT NULL
 );
 ALTER TABLE
-    `Comment` ADD PRIMARY KEY `comment_id_primary`(`id`);
+    `Post Comment` ADD PRIMARY KEY `post comment_id_primary`(`id`);
+CREATE TABLE `User`(
+    `id` CHAR(36) NOT NULL,
+    `name` VARCHAR(255) NOT NULL,
+    `phoneNumber` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `address` VARCHAR(255) NOT NULL
+);
+ALTER TABLE
+    `User` ADD PRIMARY KEY `user_id_primary`(`id`);
+CREATE TABLE `User Comments`(
+    `usersId` INT NULL,
+    `postId` INT NULL,
+    `commentId` INT NULL,
+    `replyId` INT NULL,
+    `likes` INT NULL AUTO_INCREMENT,
+    `time` TIMESTAMP NULL
+);
+CREATE TABLE `Post`(
+    `postId` INT NULL AUTO_INCREMENT,
+    `userId` INT NOT NULL,
+    `urlLink` VARCHAR(255) NULL,
+    `caption` VARCHAR(255) NULL,
+    `music` VARCHAR(255) NULL,
+    `likes` INT NULL,
+    `commentId` INT NULL,
+    `hashTagId` INT NULL,
+    `tags` VARCHAR(255) NULL,
+    `saved` TINYINT(1) NULL,
+    `time` TIMESTAMP NULL
+);
+ALTER TABLE
+    `Post` ADD PRIMARY KEY `post_postid_primary`(`postId`);
 CREATE TABLE `Album`(
     `id` CHAR(36) NOT NULL,
     `title` VARCHAR(255) NOT NULL,
@@ -49,6 +93,14 @@ CREATE TABLE `Tag Photo`(
 );
 ALTER TABLE
     `Tag Photo` ADD PRIMARY KEY `tag photo_tagid_primary`(`tagID`);
+CREATE TABLE `Likes`(
+    `postId` INT NOT NULL,
+    `likedId` INT NULL
+);
+ALTER TABLE
+    `Likes` ADD UNIQUE `likes_postid_unique`(`postId`);
+ALTER TABLE
+    `Likes` ADD UNIQUE `likes_likedid_unique`(`likedId`);
 CREATE TABLE `Location`(
     `id` CHAR(36) NOT NULL,
     `name` VARCHAR(255) NOT NULL,
@@ -64,6 +116,8 @@ CREATE TABLE `Following`(
 ALTER TABLE
     `Following` ADD PRIMARY KEY `following_id_primary`(`id`);
 ALTER TABLE
+    `Hash Tag` ADD CONSTRAINT `hash tag_postid_foreign` FOREIGN KEY(`postId`) REFERENCES `Photo`(`id`);
+ALTER TABLE
     `Tag Photo` ADD CONSTRAINT `tag photo_tagid_foreign` FOREIGN KEY(`tagID`) REFERENCES `Photo`(`id`);
 ALTER TABLE
     `User` ADD CONSTRAINT `user_id_foreign` FOREIGN KEY(`id`) REFERENCES `Photo`(`id`);
@@ -74,8 +128,24 @@ ALTER TABLE
 ALTER TABLE
     `Location` ADD CONSTRAINT `location_id_foreign` FOREIGN KEY(`id`) REFERENCES `Photo`(`id`);
 ALTER TABLE
-    `Photo` ADD CONSTRAINT `photo_id_foreign` FOREIGN KEY(`id`) REFERENCES `User`(`id`);
+    `Hash Tag` ADD CONSTRAINT `hash tag_postid_foreign` FOREIGN KEY(`postId`) REFERENCES `Saved Post`(`postId`);
+ALTER TABLE
+    `User Comments` ADD CONSTRAINT `user comments_usersid_foreign` FOREIGN KEY(`usersId`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Saved Post` ADD CONSTRAINT `saved post_postid_foreign` FOREIGN KEY(`postId`) REFERENCES `Post`(`postId`);
+ALTER TABLE
+    `Saved Post` ADD CONSTRAINT `saved post_userid_foreign` FOREIGN KEY(`userId`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Profile` ADD CONSTRAINT `profile_userid_foreign` FOREIGN KEY(`userId`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Post` ADD CONSTRAINT `post_userid_foreign` FOREIGN KEY(`userId`) REFERENCES `User`(`id`);
+ALTER TABLE
+    `Likes` ADD CONSTRAINT `likes_postid_foreign` FOREIGN KEY(`postId`) REFERENCES `Post`(`postId`);
 ALTER TABLE
     `Following` ADD CONSTRAINT `following_id_foreign` FOREIGN KEY(`id`) REFERENCES `User`(`id`);
 ALTER TABLE
-    `Comment` ADD CONSTRAINT `comment_id_foreign` FOREIGN KEY(`id`) REFERENCES `Photo`(`id`);
+    `Hash Tag` ADD CONSTRAINT `hash tag_postid_foreign` FOREIGN KEY(`postId`) REFERENCES `Photo`(`id`);
+ALTER TABLE
+    `Post Comment` ADD CONSTRAINT `post comment_id_foreign` FOREIGN KEY(`id`) REFERENCES `Photo`(`id`);
+ALTER TABLE
+    `Post Comment` ADD CONSTRAINT `post comment_photoid_foreign` FOREIGN KEY(`photoID`) REFERENCES `Photo`(`id`);
--- SQLs/Twitter-MySQL.sql
+++ SQLs/Twitter-MySQL.sql
@@ -1,4 +1,4 @@
-CREATE TABLE `users`(
+CREATE TABLE `Users`(
     `id` BIGINT NOT NULL,
     `name` VARCHAR(255) NULL,
     `email` VARCHAR(255) NULL,
@@ -9,14 +9,64 @@ CREATE TABLE `users`(
     `slug` VARCHAR(255) NULL
 );
 ALTER TABLE
-    `users` ADD PRIMARY KEY `users_id_primary`(`id`);
+    `Users` ADD PRIMARY KEY `users_id_primary`(`id`);
 ALTER TABLE
-    `users` ADD UNIQUE `users_email_unique`(`email`);
+    `Users` ADD UNIQUE `users_email_unique`(`email`);
 ALTER TABLE
-    `users` ADD INDEX `users_remember_token_index`(`remember_token`);
+    `Users` ADD INDEX `users_remember_token_index`(`remember_token`);
 ALTER TABLE
-    `users` ADD UNIQUE `users_slug_unique`(`slug`);
-CREATE TABLE `tweets`(
+    `Users` ADD UNIQUE `users_slug_unique`(`slug`);
+CREATE TABLE `Liked By`(
+    `tweet_id` INT NULL,
+    `user_id` INT NULL
+);
+CREATE TABLE `Draft Scheduled Tweets`(
+    `userid` INT NULL,
+    `draft_tweet_id` INT NULL,
+    `scheduled_tweet_id` INT NULL
+);
+CREATE TABLE `Posts`(
+    `tweet_id` INT NULL AUTO_INCREMENT,
+    `tweet_user_id` INT NULL,
+    `tweet_content` VARCHAR(255) NULL,
+    `like` INT NULL AUTO_INCREMENT,
+    `retweet` INT NULL AUTO_INCREMENT,
+    `reply` INT NULL AUTO_INCREMENT,
+    `views` INT NULL AUTO_INCREMENT,
+    `createdAt` TIMESTAMP NULL
+);
+ALTER TABLE
+    `Posts` ADD PRIMARY KEY `posts_tweet_user_id_primary`(`tweet_user_id`);
+CREATE TABLE `Languages`(
+    `lang_id` INT NULL,
+    `lang_name` VARCHAR(255) NULL
+);
+CREATE TABLE `Trending`(
+    `Tweets_id` INT NULL,
+    `Tweet_hashtag` VARCHAR(255) NULL
+);
+CREATE TABLE `Twittercircle`(
+    `count` INT NULL AUTO_INCREMENT,
+    `user_id` INT NULL,
+    `friend_id` INT NULL
+);
+CREATE TABLE `Replies`(
+    `reply_count` INT NULL AUTO_INCREMENT,
+    `tweet_id` INT NULL,
+    `user_id` INT NULL,
+    `reply_content` VARCHAR(255) NULL,
+    `mention_id` INT NULL,
+    `like` INT NULL AUTO_INCREMENT,
+    `retweet` INT NULL AUTO_INCREMENT,
+    `reply` INT NULL AUTO_INCREMENT,
+    `views` INT NULL AUTO_INCREMENT,
+    `createdAt` TIMESTAMP NULL
+);
+CREATE TABLE `User Languages`(
+    `id` INT NULL,
+    `language` VARCHAR(255) NULL
+);
+CREATE TABLE `Tweets`(
     `id` BIGINT NOT NULL,
     `content` VARCHAR(255) NULL,
     `user_id` INT NULL,
@@ -24,10 +74,37 @@ CREATE TABLE `tweets`(
     `updated_at` DATETIME NOT NULL
 );
 ALTER TABLE
-    `tweets` ADD INDEX `tweets_user_id_created_at_index`(`user_id`, `created_at`);
+    `Tweets` ADD INDEX `tweets_user_id_created_at_index`(`user_id`, `created_at`);
 ALTER TABLE
-    `tweets` ADD PRIMARY KEY `tweets_id_primary`(`id`);
-CREATE TABLE `relationships`(
+    `Tweets` ADD PRIMARY KEY `tweets_id_primary`(`id`);
+CREATE TABLE `UserAccount`(
+    `id` INT NULL AUTO_INCREMENT,
+    `Name` VARCHAR(255) NULL,
+    `username` VARCHAR(255) NOT NULL,
+    `email` VARCHAR(255) NOT NULL,
+    `password` VARCHAR(255) NULL,
+    `BirthDate` DATE NULL,
+    `Birthmonth` INT NULL,
+    `Birthyear` INT NULL,
+    `category` VARCHAR(255) NULL,
+    `phonenumber` INT NOT NULL,
+    `profilepicture_url` VARCHAR(255) NULL,
+    `bio` VARCHAR(255) NULL,
+    `website` VARCHAR(255) NULL,
+    `Location` VARCHAR(255) NULL,
+    `verified` TINYINT(1) NULL,
+    `createdAt` TIMESTAMP NULL,
+    `two_auth` TINYINT(1) NULL
+);
+ALTER TABLE
+    `UserAccount` ADD PRIMARY KEY `useraccount_id_primary`(`id`);
+ALTER TABLE
+    `UserAccount` ADD UNIQUE `useraccount_username_unique`(`username`);
+ALTER TABLE
+    `UserAccount` ADD UNIQUE `useraccount_email_unique`(`email`);
+ALTER TABLE
+    `UserAccount` ADD UNIQUE `useraccount_phonenumber_unique`(`phonenumber`);
+CREATE TABLE `Relationships`(
     `id` BIGINT NOT NULL,
     `follower_id` INT NULL,
     `followed_id` INT NULL,
@@ -35,16 +112,61 @@ CREATE TABLE `relationships`(
     `updated_at` DATETIME NOT NULL
 );
 ALTER TABLE
-    `relationships` ADD UNIQUE `relationships_follower_id_followed_id_unique`(`follower_id`, `followed_id`);
+    `Relationships` ADD UNIQUE `relationships_follower_id_followed_id_unique`(`follower_id`, `followed_id`);
 ALTER TABLE
-    `relationships` ADD PRIMARY KEY `relationships_id_primary`(`id`);
+    `Relationships` ADD PRIMARY KEY `relationships_id_primary`(`id`);
 ALTER TABLE
-    `relationships` ADD INDEX `relationships_follower_id_index`(`follower_id`);
+    `Relationships` ADD INDEX `relationships_follower_id_index`(`follower_id`);
 ALTER TABLE
-    `relationships` ADD INDEX `relationships_followed_id_index`(`followed_id`);
+    `Relationships` ADD INDEX `relationships_followed_id_index`(`followed_id`);
+CREATE TABLE `Quote Tweet`(
+    `tweet_id` INT NULL,
+    `user_id` INT NULL
+);
+CREATE TABLE `Login Activity`(
+    `login_count` INT NULL AUTO_INCREMENT,
+    `user_id` INT NULL,
+    `login_time` TIMESTAMP NULL
+);
+CREATE TABLE `Retweet`(
+    `tweet_id` INT NULL,
+    `user_id` INT NULL
+);
 ALTER TABLE
-    `tweets` ADD CONSTRAINT `tweets_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `users`(`id`);
+    `Posts` ADD CONSTRAINT `posts_tweet_id_foreign` FOREIGN KEY(`tweet_id`) REFERENCES `UserAccount`(`id`);
 ALTER TABLE
-    `relationships` ADD CONSTRAINT `relationships_follower_id_foreign` FOREIGN KEY(`follower_id`) REFERENCES `users`(`id`);
+    `Liked By` ADD CONSTRAINT `liked by_tweet_id_foreign` FOREIGN KEY(`tweet_id`) REFERENCES `Tweets`(`id`);
 ALTER TABLE
-    `relationships` ADD CONSTRAINT `relationships_followed_id_foreign` FOREIGN KEY(`followed_id`) REFERENCES `users`(`id`);
+    `Draft Scheduled Tweets` ADD CONSTRAINT `draft scheduled tweets_userid_foreign` FOREIGN KEY(`userid`) REFERENCES `Replies`(`reply_count`);
+ALTER TABLE
+    `Tweets` ADD CONSTRAINT `tweets_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+ALTER TABLE
+    `User Languages` ADD CONSTRAINT `user languages_id_foreign` FOREIGN KEY(`id`) REFERENCES `Languages`(`lang_id`);
+ALTER TABLE
+    `Users` ADD CONSTRAINT `users_id_foreign` FOREIGN KEY(`id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Relationships` ADD CONSTRAINT `relationships_follower_id_foreign` FOREIGN KEY(`follower_id`) REFERENCES `Users`(`id`);
+ALTER TABLE
+    `Retweet` ADD CONSTRAINT `retweet_tweet_id_foreign` FOREIGN KEY(`tweet_id`) REFERENCES `Tweets`(`id`);
+ALTER TABLE
+    `Languages` ADD CONSTRAINT `languages_lang_id_foreign` FOREIGN KEY(`lang_id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Relationships` ADD CONSTRAINT `relationships_followed_id_foreign` FOREIGN KEY(`followed_id`) REFERENCES `Users`(`id`);
+ALTER TABLE
+    `Draft Scheduled Tweets` ADD CONSTRAINT `draft scheduled tweets_userid_foreign` FOREIGN KEY(`userid`) REFERENCES `Tweets`(`id`);
+ALTER TABLE
+    `Liked By` ADD CONSTRAINT `liked by_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Login Activity` ADD CONSTRAINT `login activity_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `Users`(`id`);
+ALTER TABLE
+    `Retweet` ADD CONSTRAINT `retweet_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Twittercircle` ADD CONSTRAINT `twittercircle_user_id_foreign` FOREIGN KEY(`user_id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Trending` ADD CONSTRAINT `trending_tweets_id_foreign` FOREIGN KEY(`Tweets_id`) REFERENCES `Tweets`(`id`);
+ALTER TABLE
+    `User Languages` ADD CONSTRAINT `user languages_id_foreign` FOREIGN KEY(`id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Quote Tweet` ADD CONSTRAINT `quote tweet_tweet_id_foreign` FOREIGN KEY(`tweet_id`) REFERENCES `UserAccount`(`id`);
+ALTER TABLE
+    `Tweets` ADD CONSTRAINT `tweets_id_foreign` FOREIGN KEY(`id`) REFERENCES `UserAccount`(`id`);
